c687cc14790516c7a831e54723273e09ac5b607c
Akhil7800 <akhilranjanit7800@gmail.com>
2025-08-06 16:52:58 +0530

    add user and video models

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+// app.on("error", (error) => {
+//   console.log("ERROR:", error);
+//   throw error;
+// });
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`youtube Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+  });
 
 /*
 import express from "express";
